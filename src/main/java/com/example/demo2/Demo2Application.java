7628151cd7b7d0ea03339d78fcec70a30cad6c05
GlassySk <90904055+GlassySk@users.noreply.github.com>
2023-06-19 23:31:36 +0800

    kk

---
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo2;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@MapperScan("com.example.demo2.mapper") //扫描的mapper
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+}
